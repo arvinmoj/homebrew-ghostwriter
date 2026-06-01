b2730f41adbed91fe93a5b91e92fe3b4401044da
Arvinmoj <arvinmojaverian@hotmail.com>
2026-06-01 14:49:45 +0330

    fix: update SHA256 for v0.5.0 release

--- Casks/ghostwriter.rb
+++ Casks/ghostwriter.rb
@@ -6,11 +6,11 @@ cask "ghostwriter" do
 
   on_arm do
     url "https://github.com/arvinmoj/GhostWriter/releases/download/v#{version}/GhostWriter_#{version}_aarch64.dmg"
-    sha256 "<placeholder-run-release-first>"
+    sha256 "92740f1f4adee8a1c1f6741bcb0bc3476074e2e0492701bff9d38113fe87e7e3"
   end
   on_intel do
     url "https://github.com/arvinmoj/GhostWriter/releases/download/v#{version}/GhostWriter_#{version}_x64.dmg"
-    sha256 "<placeholder-run-release-first>"
+    sha256 "4bbe3b3de800d4e2b819222f06c60b1c7c59a771d2c13b9529cadec944c8fdc2"
   end
 
   app "GhostWriter.app"
