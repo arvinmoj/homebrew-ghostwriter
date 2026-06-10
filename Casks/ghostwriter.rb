75493e5504900e93037d3cc8f9e1c2d3fd2926c4
arvinmoj <52661749+arvinmoj@users.noreply.github.com>
2026-06-10 08:30:07 +0000

    Update GhostWriter to v0.10.0

--- Casks/ghostwriter.rb
+++ Casks/ghostwriter.rb
@@ -1,16 +1,16 @@
 cask "ghostwriter" do
-  version "0.9.0"
+  version "0.10.0"
   name "GhostWriter"
   desc "Universal AI Text Refiner - System-wide text transformation anywhere on your computer"
   homepage "https://github.com/arvinmoj/GhostWriter"
 
   on_arm do
     url "https://github.com/arvinmoj/GhostWriter/releases/download/v#{version}/GhostWriter_#{version}_aarch64.dmg"
-    sha256 "76a929ec1618691ae37753d352653aa4adf39a00fe6c7bd419fdb410a684a9e3"
+    sha256 "6c03484ea9f8d7e7035d9ff58134cd6a8bea9e2e143f5e43c1b5bd16cff4817e"
   end
   on_intel do
     url "https://github.com/arvinmoj/GhostWriter/releases/download/v#{version}/GhostWriter_#{version}_x64.dmg"
-    sha256 "3122c0e5d091ac914ee135f21abc97a76ce2b745e89cd0efc15e86bcc83745ae"
+    sha256 "5446063a81c18c123343a69d6df34ccf5dce1d014a9d38a13fa9bbd9b4712a25"
   end
 
   app "GhostWriter.app"
